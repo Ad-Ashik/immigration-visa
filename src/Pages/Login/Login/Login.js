0e87e4e2bf52fb51e6fc5447ddc481f1ab7d5110
AIA <adashik585@gmail.com>
2022-04-18 19:08:10 +0600

    used toast reset password

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import SoccailLogin from '../SocailLogin/SocailLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+    const emailRef = useRef('');
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(
+        auth
+    );
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -45,19 +51,39 @@ const Login = () => {
 
         signInWithEmailAndPassword(email, password);
     }
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast.success('please check your email', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+        else {
+            toast.error('please entar your email', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+    }
+
     return (
         <div className='w-50 mx-auto border border-3 mt-5 p-4 rounded-3'>
             <h2 className='text-primary text-center'>Please Login</h2>
             <Form onSubmit={formSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" name="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name="password" placeholder="Password" required />
                 </Form.Group>
+                <p>Forget your password? <Link to="" className='text-primary text-decoration-underline' onClick={resetPassword}>
+                    Reset Password
+                </Link>
+                </p>
                 <Button className='w-25 my-3' variant="primary" type="submit">
                     Login
                 </Button>
@@ -68,6 +94,7 @@ const Login = () => {
                 </Link>
                 </p>
                 <SoccailLogin></SoccailLogin>
+                <ToastContainer />
             </Form>
         </div>
     );
